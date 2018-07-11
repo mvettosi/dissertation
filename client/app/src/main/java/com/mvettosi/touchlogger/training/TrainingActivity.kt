98d616f75acf2ecbfa06db195b10c76430a349a9
mvettosi <matteo.vettosi@gmail.com>
2018-07-11 21:30:23 +0100

    Switched from ugly alert to new pin button

--- client/app/src/main/java/com/mvettosi/touchlogger/training/TrainingActivity.kt
+++ client/app/src/main/java/com/mvettosi/touchlogger/training/TrainingActivity.kt
@@ -1,13 +1,12 @@
 package com.mvettosi.touchlogger.training
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import com.mvettosi.touchlogger.R
 import com.mvettosi.touchlogger.listener.SensorDataListener
 import kotlinx.android.synthetic.main.activity_main.*
@@ -34,11 +33,6 @@ class TrainingActivity : AppCompatActivity() {
 //        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        newPinAlert(null)
-    }
-
     override fun onPause() {
         super.onPause()
         if (sensorDataListener.isRecording) {
@@ -56,40 +50,15 @@ class TrainingActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.action_settings -> {
                 startActivity(Intent(this, SettingsActivity::class.java))
-                return true
+                true
+            }
+            R.id.action_newpin -> {
+                showNewPin()
+                sensorDataListener.startRecording()
+                true
             }
         }
         return false
-    }
-
-    // Activity Logic
-    private fun newPinAlert(previousSucc: Boolean?) {
-        var msg = ""
-        when (previousSucc) {
-            null -> {
-                sensorDataListener.discardRecording()
-                msg += ""
-            }
-            true -> {
-                sensorDataListener.stopRecording(samplePin.text.toString())
-                msg += "Collected!\n"
-            }
-            false -> {
-                sensorDataListener.discardRecording()
-                msg += "Discarded!\n"
-            }
-        }
-        msg += "Do you want to insert a new pin?"
-        val alertDialog = AlertDialog.Builder(this@TrainingActivity)
-                .setMessage(msg)
-                .setPositiveButton("Yes") { _: DialogInterface, _: Int ->
-                    showNewPin()
-                    sensorDataListener.startRecording()
-                }
-                .setNegativeButton("No") { _: DialogInterface, _: Int ->
-                    closeApplication()
-                }.create()
-        alertDialog.show()
     }
 
     private fun showNewPin() {
@@ -97,24 +66,25 @@ class TrainingActivity : AppCompatActivity() {
         samplePin.text = Random().nextInt(10000).toString().padStart(4, '0')
     }
 
-    private fun closeApplication() {
-        val homeIntent = Intent(Intent.ACTION_MAIN)
-        homeIntent.addCategory(Intent.CATEGORY_HOME)
-        homeIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-        startActivity(homeIntent)
-    }
-
     fun addDigit(view: View) {
-        val digit = view.tag.toString()
-        val currentText = typePin.text.toString()
-        val newText = currentText + digit
-        typePin.setText(newText)
-        if (newText.length == samplePin.text.length) {
-            if (newText == samplePin.text) {
-                newPinAlert(true)
-            } else {
-                newPinAlert(false)
+        if (sensorDataListener.isRecording) {
+            val digit = view.tag.toString()
+            val currentText = typePin.text.toString()
+            val newText = currentText + digit
+            typePin.setText(newText)
+            if (newText.length == samplePin.text.length) {
+                if (newText == samplePin.text) {
+                    sensorDataListener.stopRecording(samplePin.text.toString())
+                    toast("Collected!")
+                } else {
+                    sensorDataListener.discardRecording()
+                    toast("Discarded!")
+                }
             }
         }
+    }
+
+    fun toast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 }
